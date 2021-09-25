9d179e2ab15fe539c1c9a32594e463994f2a0e14
jiangnanhuabei <13343257639@163.com>
2021-09-25 18:00:40 +0800

    swiper

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,12 +1,56 @@
 import React from "react";
+import { Carousel } from 'antd-mobile';
+import axios from "axios";
 
+export default class Index extends React.Component {
+    state = {
+        // 轮播图状态数据
+        swipers: []
+        // data: ['1', '2', '3'],
+        // imgHeight: 176,
+    }
+    // 获取轮播图数据的方法
+    async getSwipers() {
+        const res = await axios.get("http://localhost:8080/home/swiper")
+        this.setState(() => {
+            return ({
+                swipers: res.data.body
+            })
+        })
+    }
 
-export default class Index extends React.Component{
-    render(){
-        return(
-            <div>
-                这是首页子组件
+    componentDidMount() {
+        this.getSwipers()
+    }
+    // 轮播图
+    renderSwipers() {
+        return (
+            this.state.swipers.map(item => (
+                <a
+                    key={item.it}
+                    href="http://www.alipay.com"
+                    style={{ display: 'inline-block', width: '100%', height: 212 }}
+                >
+                    <img
+                        src={`http://127.0.0.1:8080${item.imgSrc}`}
+                        alt=""
+                        style={{ width: '100%', verticalAlign: 'top' }}
+                    />
+                </a>
+            )))
+    }
+
+    render() {
+        return (
+            <div className='index'>
+                <Carousel
+                    autoplay={false}
+                    infinite
+                    autoplayInterval={5000}
+                >
+                    {this.renderSwipers()}
+                </Carousel>
             </div>
-        )
+        );
     }
 }
